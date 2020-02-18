947562e52e722ceb197197017f194bef9d5220ef
blockheads <nicholaspueschel@gmail.com>
2020-02-17 20:08:55 -0500

    Added createtable,altertable, and drop table skeletons

--- src/ddl/DDLParser.java
+++ src/ddl/DDLParser.java
@@ -6,6 +6,10 @@ package ddl;
 
 public class DDLParser implements IDDLParser {
 
+    private final String createTableStatement = "create table";
+    private final String alterTableStatement = "alter table";
+    private final String dropTableStatement = "drop table";
+
     /**
      * This will create an instance of this parser and return it.
      * @return an instance of a IDDLParser
@@ -16,6 +20,40 @@ public class DDLParser implements IDDLParser {
 
     @Override
     public void parseDDLstatement(String statement) throws DDLParserException {
+
+        // convert to lowercase
+        statement = statement.toLowerCase();
+        // strip leading white space
+        statement = statement.stripLeading();
+
+        // we check if we begin with a valid statement
+        if(statement.startsWith(createTableStatement)){
+            String args = statement.substring(createTableStatement.length());
+            parseCreateTableStatement(args);
+        }
+        else if(statement.startsWith(alterTableStatement)){
+            String args = statement.substring(alterTableStatement.length());
+            parseAlterTableStatement(args);
+        }
+        else if(statement.startsWith(dropTableStatement)){
+            String args = statement.substring(dropTableStatement.length());
+            parseDropTableStatement(args);
+        }
+        else {
+            throw new DDLParserException(DDLParserException.INVALID_STATEMENT);
+        }
+
+    }
+
+    private void parseCreateTableStatement(String args){
+
+    }
+
+    private void parseAlterTableStatement(String args){
+
+    }
+
+    private void parseDropTableStatement(String args){
 
     }
 }
--- src/ddl/DDLParserException.java
+++ src/ddl/DDLParserException.java
@@ -5,4 +5,6 @@ public class DDLParserException extends Exception {
     public DDLParserException(String message) {
         super(message);
     }
+
+    public final static String INVALID_STATEMENT = "You have entered in a invalid statement not supported by the database.";
 }
